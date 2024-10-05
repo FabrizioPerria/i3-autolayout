c132b006b058aebcbf67e6b0f1b6c732176de16f
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-10-05 14:13:31 +0200

    split install functions

--- install.sh
+++ install.sh
@@ -1,59 +1,85 @@
 #!/bin/bash
 
-if [[ "${1}" != "--prefix" ]] ; then
-    echo -e "Usage:\n  ${0} --prefix PREFIX"
-    exit -1
+set -e
+
+PREFIX="/usr"
+BUILD=false
+INSTALL=false
+REFRESH=false
+
+# Parse command line arguments
+while (("$#")); do
+    case "$1" in
+    --prefix)
+        PREFIX="$2"
+        shift 2
+        ;;
+    --build)
+        BUILD=true
+        shift
+        ;;
+    --install)
+        INSTALL=true
+        shift
+        ;;
+    --refresh)
+        REFRESH=true
+        shift
+        ;;
+    *)
+        echo "Error: Invalid argument"
+        exit 1
+        ;;
+    esac
+done
+
+if [[ "${BUILD}" == "true" ]]; then
+    if [[ -z "${SUDO_USER}" ]]; then
+        THEUSER="${USER}"
+        cargo build --release
+    else
+        THEUSER="${SUDO_USER}"
+        echo "Detected as sudo. Build as normal user: ${SUDO_USER}"
+        su "${SUDO_USER}" -c "cargo build --release"
+    fi
 fi
 
-prefix=$(echo "${2}" | sed 's:/*$::')
+PREFIX="${PREFIX%/}"
 
-if [[ -z "${prefix}" ]] ; then
-    echo -e "Missing PREFIX operand"
-    exit -1
+if [[ "${INSTALL}" == "true" ]]; then
+    echo "Install in '${PREFIX}'" &&
+        mkdir -p "${PREFIX}" &&
+        mkdir -p "${PREFIX}/bin" &&
+        mkdir -p "${PREFIX}/lib/systemd/user" &&
+        mkdir -p "${PREFIX}/env" &&
+        install ./target/release/i3-autolayout "${PREFIX}/bin" &&
+        sed "s#ExecStart=i3-autolayout#ExecStart=${PREFIX}/bin/i3-autolayout#g" \
+            ./systemd/i3-autolayout.service \
+            >"${PREFIX}/lib/systemd/user/i3-autolayout.service" &&
+        sed "s#AUTOLAYOUT_BIN_DIR=%%%#AUTOLAYOUT_BIN_DIR=${PREFIX}/bin#g" \
+            ./env/env \
+            >"${PREFIX}/env/env"
 fi
 
+if [[ "${REFRESH}" == "true" ]]; then
+    THEHOME=$(eval echo "~${THEUSER}")
 
-if [[ -z "${SUDO_USER}" ]] ; then
-    THEUSER="${USER}"
-    cargo build --release
-else
-    THEUSER="${SUDO_USER}"
-    echo "Detected as sudo. Build as normal user: ${SUDO_USER}"
-    su "${SUDO_USER}" -c "cargo build --release"
-fi
-
-echo "Install in '${prefix}'" && \
-    mkdir -p "${prefix}" && \
-    mkdir -p "${prefix}/bin" && \
-    mkdir -p "${prefix}/lib/systemd/user" && \
-    mkdir -p "${prefix}/env" && \
-    install ./target/release/i3-autolayout "${prefix}/bin" && \
-    sed "s#ExecStart=i3-autolayout#ExecStart=${prefix}/bin/i3-autolayout#g" \
-        ./systemd/i3-autolayout.service \
-        > "${prefix}/lib/systemd/user/i3-autolayout.service" && \
-    sed "s#AUTOLAYOUT_BIN_DIR=%%%#AUTOLAYOUT_BIN_DIR=${prefix}/bin#g" \
-        ./env/env \
-        > "${prefix}/env/env"
-
-THEHOME=$(eval echo "~${THEUSER}")
-
-read -r -p "Source environment for user '${THEUSER}'? [y/N]: " ans
-case "${ans}" in
+    read -r -p "Source environment for user '${THEUSER}'? [y/N]: " ans
+    case "${ans}" in
     [yY])
-        line=". \"${prefix}/env/env\""
+        line=". \"${PREFIX}/env/env\""
         envs=".profile .bashrc"
 
         for vv in ${envs}; do
             file="${THEHOME}/${vv}"
             if [[ -f "${file}" ]]; then
-                if ! grep -Fxq "${line}" "${file}"
-                then
-                    echo -e "\n${line}" >> "${file}"
+                if ! grep -Fxq "${line}" "${file}"; then
+                    echo -e "\n${line}" >>"${file}"
                     echo "  Sourced file '${file}'"
                 fi
             fi
         done
         ;;
-    *)
-        ;;
-esac
+    *) ;;
+    esac
+fi
